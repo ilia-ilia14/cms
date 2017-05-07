15fb32595e8ec1f768d79a57d07e0b7dceac2a90
ilia-ilia14 <simon.ilia.sd@gmail.com>
2017-05-07 01:23:21 -0600

    assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { HeaderComponent } from './header.component';
+import { ContactItemComponent } from './contacts/contact-list/contact-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ContactListComponent,
+    ContactDetailComponent,
+    HeaderComponent,
+    ContactItemComponent
   ],
   imports: [
     BrowserModule,
